e687bf1ffe2b58f4c2cd8d3dd315412d82cf81bb
Rahul Chaudhary <rahul.chaudhary.3100@gmail.com>
2021-04-23 23:39:55 +0530

    User Registration

--- mySite/blog/views.py
+++ mySite/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+import random
+
+colors = ['red', 'yellow', 'blue', 'purple']
+color = random.choice(colors)
+
 
 def home(request):
     context = {
-        'posts': Post.objects.all()
+        'posts': Post.objects.all(),
+        'color': color
     }
     return render(request, "blog/home.html", context)
 
@@ -16,3 +22,6 @@ def about(request):
 
 def login(request):
     return render(request, "blog/login.html")
+
+def test(request):
+    return render(request, "blog/test.html")
--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_tailwind',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
